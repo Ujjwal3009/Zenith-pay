cdac32aa95ab87a579bf2007c188fa94a3737ef1
Ujjwal Kumar <50870185+Ujjwal3009@users.noreply.github.com>
2025-06-22 00:16:17 +0530

    generated initial setup

---
+++ backend/secure-vault-mock/src/main/java/com/zenith/secure_vault_mock/SecureVaultMockApplication.java
@@ -0,0 +1,13 @@
+package com.zenith.secure_vault_mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureVaultMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureVaultMockApplication.class, args);
+	}
+
+}
---
+++ backend/secure-vault-mock/src/test/java/com/zenith/secure_vault_mock/SecureVaultMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenith.secure_vault_mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureVaultMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
